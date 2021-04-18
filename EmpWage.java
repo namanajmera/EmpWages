4faa6b06c11d6a9b9392222266aead2830dfb7c8
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 12:03:01 +0530

    Modified: to UC3 Problem.

--- EmpWage.java
+++ EmpWage.java
@@ -1,16 +1,18 @@
 class EmpWage {
-    static final int isPresent = 1;
-    static final int empPerRate = 20;
+    public static final int isPresent = 1;
+    public static final int empPerRate = 20;
+    public static final int isPartTime = 2;
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         // System.out.println();
         int empPerHour = 0;
         int empWage = 0;
         if (empCheck == isPresent) {
             empPerHour = 8;
+        } else if (empCheck == isPartTime) {
+            empPerHour = 4;
         }
         empWage = empPerHour * empPerRate;
         System.out.println("Emp Wage:- " + empWage);
