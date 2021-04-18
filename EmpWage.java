d923d08101a29d275776b9b2c008ce43e3b332cb
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 12:12:42 +0530

    Modified: to UC6 Problem.

--- EmpWage.java
+++ EmpWage.java
@@ -3,31 +3,29 @@ class EmpWage {
     public static final int empPerRate = 20;
     public static final int isPartTime = 2;
     public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 10;
 
     public static void main(String[] args) {
 
         // System.out.println();
-        int empPerHour = 0;
-        int empWage = 0;
-        int totalWage = 0;
-
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        int empHrs = 0, totalEmphrs = 0, totalWorkingDays = 0;
+        while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+            totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-
             switch (empCheck) {
             case isPresent:
-                empPerHour = 8;
+                empHrs = 8;
                 break;
             case isPartTime:
-                empPerHour = 4;
+                empHrs = 4;
                 break;
             default:
-                empPerHour = 0;
+                empHrs = 0;
             }
-            empWage = empPerHour * empPerRate;
-            totalWage += empWage;
-            System.out.println("Emp Wage:- " + empWage);
+            totalEmphrs += empHrs;
+            System.out.println("Days:- " + totalWorkingDays + " EmpHrs:- " + empHrs);
         }
-        System.out.print("Total wage is:- " + totalWage);
+        int totalEmpWage = totalEmphrs + empPerRate;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
     }
 }
