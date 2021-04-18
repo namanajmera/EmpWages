efdc4110bde5a07657cfbbf0ae4357f136864895
Naman <cool.naman.ajmera@gmail.com>
2021-04-18 12:06:42 +0530

    Modified: to UC5 Problem.

--- EmpWage.java
+++ EmpWage.java
@@ -2,24 +2,32 @@ class EmpWage {
     public static final int isPresent = 1;
     public static final int empPerRate = 20;
     public static final int isPartTime = 2;
+    public static final int NUM_OF_WORKING_DAYS = 20;
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
         // System.out.println();
         int empPerHour = 0;
         int empWage = 0;
-        switch (empCheck) {
-        case isPresent:
-            empPerHour = 8;
-            break;
-        case isPartTime:
-            empPerHour = 4;
-            break;
-        default:
-            empPerHour = 0;
+        int totalWage = 0;
+
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+
+            switch (empCheck) {
+            case isPresent:
+                empPerHour = 8;
+                break;
+            case isPartTime:
+                empPerHour = 4;
+                break;
+            default:
+                empPerHour = 0;
+            }
+            empWage = empPerHour * empPerRate;
+            totalWage += empWage;
+            System.out.println("Emp Wage:- " + empWage);
         }
-        empWage = empPerHour * empPerRate;
-        System.out.println("Emp Wage:- " + empWage);
+        System.out.print("Total wage is:- " + totalWage);
     }
 }
