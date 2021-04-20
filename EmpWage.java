b51c7ed22458a03f807b1b32007572d642536035
Naman <cool.naman.ajmera@gmail.com>
2021-04-20 08:30:00 +0530

    Modified to OOps Concept

--- EmpWage.java
+++ EmpWage.java
@@ -8,6 +8,19 @@ class EmpWage {
     public static void main(String[] args) {
 
         // System.out.println();
+        EmpComputation empComputation = new EmpComputation();
+        empComputation.CalculateWageWorkingHour(empPerRate, NUM_OF_WORKING_DAYS, MAX_HRS_IN_MONTH);
+    }
+}
+
+class EmpComputation {
+    public static final int isPresent = 1;
+    // public static final int empPerRate = 20;
+    public static final int isPartTime = 2;
+    // public static final int NUM_OF_WORKING_DAYS = 20;
+    // public static final int MAX_HRS_IN_MONTH = 10;
+
+    void CalculateWageWorkingHour(int empPerRate, int NUM_OF_WORKING_DAYS, int MAX_HRS_IN_MONTH) {
         int empHrs = 0, totalEmphrs = 0, totalWorkingDays = 0;
         while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
